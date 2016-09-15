37dcda2b68ac29f01e66b50b5437803482fdbb71
Ness Morris <karplusstrong@gmail.com>
2016-09-14 23:20:45 -0700

    add mutators to NamedElement

--- src/NamedElement.h
+++ src/NamedElement.h
@@ -7,5 +7,7 @@
 class NamedElement{
     public:
         virtual std::string name() = 0;
+        virtual std::string name(std::string name) = 0;
         virtual std::string description() = 0;
+        virtual std::string description(std::string description) = 0;
 }
