35cde6a2d8c1a54bb520a3471055b4ee4c4ac60f
Ness Morris <karplusstrong@gmail.com>
2016-09-14 22:49:33 -0700

    add doc comments

--- src/floor_tile.cpp
+++ src/floor_tile.cpp
@@ -1,5 +1,10 @@
 #include "floor_tile.h"
 
+/**
+ * Construct a FloorTile with the given X and Y coordinates.
+ * @param[in] x
+ * @param[in] y
+ */
 FloorTile::FloorTile(int x, int y){
     m_symbol = '#';
     m_x = x;
@@ -7,6 +12,12 @@ FloorTile::FloorTile(int x, int y){
     m_depth = 100;
 }
 
+/**
+ * Construct a FloorTile with the given X and Y coordinates, and the given symbol.
+ * @param[in] x
+ * @param[in] y
+ * @param[in] symbol The symbol to display
+ */
 FloorTile::FloorTile(int x, int y, char symbol){
     m_symbol = symbol;
     m_x = x;
@@ -14,21 +25,39 @@ FloorTile::FloorTile(int x, int y, char symbol){
     m_depth = 100;
 }
 
+/**
+ * Check if the FloorTile is visible.
+ * @retval bool
+ */
 bool FloorTile::visible(){
     // TODO: implement line-of-sight
     return true;
 }
 
+/**
+ * Check if the FloorTile location is known
+ * @retval bool
+ */
 bool FloorTile::known(){
     // TODO: implement player memory
     return true;
 }
 
+/**
+ * Check if movement through the FloorTile is prohibited
+ * @retval bool
+ */
 bool FloorTile::blocksMovement(){
     /* always traversable */
     return false;
 }
 
+/**
+ * Move the FloorTile to the given X and Y coordinates.
+ * @param x
+ * @param y
+ * @retval bool `true` if the move succeeded, `false` otherwise.
+ */
 bool FloorTile::move(int x, int y){
     /* cannot move */
     return false;
--- src/item.h
+++ src/item.h
@@ -8,14 +8,21 @@ using namespace std;
 
 class Item : OnscreenElement{
     public:
+        /** Construct item with optional name and description */
         Item(string name="", string desc="");
+        /** Get name of item @retval string*/
         string name(){ return m_name; }
+        /** Set name of item @param name @retval string*/
         string name(string name){ m_name = name; return m_name; }
 
+        /** Get item description @retval string */
         string description(){ return m_desc; }
+        /** Set item description @param desc @retval string */
         string description(string desc){ m_desc = desc; return m_desc; }
 
+        /** Get item weight @retval int */
         int weight(){ return m_weight; }
+        /** Set item weight @param weight @retval int */
         int weight(int weight){ m_weight = weight; return m_weight; }
 
     private:
