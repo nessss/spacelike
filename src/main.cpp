738ca88c3da0dc76b84bc56e364c7d59b0fc7341
Ness Morris <ness_morris@wayforward.com>
2016-09-21 12:12:29 -0700

    remove debug printing

--- src/main.cpp
+++ src/main.cpp
@@ -35,34 +35,26 @@ int main(int argc, char *argv[]){
 
     zone.loadMap("../data/test.map");
 
-    std::cerr << "zone constructed" << std::endl;
 
     output.init(options, &zone);
     output.zone(&zone);
-    std::cerr << "zone output set" << std::endl;
 
     zone.addElement(&player);
+
     Output::getInstance().gameWindowCursorPosition(player.x(), player.y());
-    std::cerr << "player added to zone" << std::endl;
 
     Item slimeMold;
     zone.addElement(&slimeMold);
-    std::cerr << "slime mold added to zone" << std::endl;
 
     slimeMold.name("Slime mold");
     slimeMold.description("A tasty, juicy, nutritious slime mold. Your mouth waters just thinking about it.");
-    std::cerr << "slime mold info set" << std::endl;
     slimeMold.move(12, 14);
-    std::cerr << "slime mold moved" << std::endl;
 
     output.refreshGameWindow();
-    std::cerr << "game window refreshed" << std::endl;
 
-    cerr << (*(zone.topmostElements()->begin()))->symbol() << std::endl;
 
     while(true){
         input.processInput();
-        std::cerr << "input parsed" << std::endl;
 
         output.refreshGameWindow();
     }
