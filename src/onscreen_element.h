d73563b7dfc0237c796a9a82eb8589d50906a608
Ness Morris <ness_morris@wayforward.com>
2016-09-21 17:31:41 -0700

    formatting

--- src/onscreen_element.h
+++ src/onscreen_element.h
@@ -23,15 +23,19 @@ class OnscreenElement{
 
         //! Return unique ID for this element
         std::size_t id() const { return m_id; }
+
         //! Return on-screen representation
         virtual char symbol() const { return m_symbol; }
         virtual char symbol(char newSymbol){ m_symbol = newSymbol; return m_symbol; }
+
         //! Return X coordinate in world space
         int x() const { return m_x; }
         int x(int x){ m_x = x; return m_x; }
+
         //! Return Y coordinate in world space
         int y() const { return m_y; }
         int y(int y){ m_y = y; return m_y; }
+
         //! Return draw order depth of element
         int depth() const { return m_depth; }
         int depth(int newDepth){ m_depth = newDepth; return m_depth; }
@@ -42,18 +46,21 @@ class OnscreenElement{
         Tile* tile() const { return m_tile; } //!< Set current tile
         Tile* tile(Tile* tile){ m_tile = tile; return m_tile; } //!< Get current tile
 
-
     protected:
         //! Constructor
         OnscreenElement();
+
         //! Unique ID
         std::size_t m_id;
+
         //! On-screen representation
         char m_symbol;
+
         //@{
         /// World space coordinate
         int m_x, m_y;
         //@}
+
         //! Draw order depth
         int m_depth;
 
